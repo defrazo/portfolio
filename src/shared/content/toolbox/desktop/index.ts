7e36d5546519175e66b3f8facd1298ec3e5deb94
Eugene Letunov <defrazo@yandex.ru>
2026-06-22 22:26:24 +0500

    feat: updated content (portfolio and toolbox images)

---
+++ src/shared/content/toolbox/desktop/index.ts
@@ -0,0 +1,7 @@
+import THomeD from './t_home_d.webp';
+import TLinkD from './t_links_d.webp';
+import TRenamerD from './t_renamer_d.webp';
+import TSettingsD from './t_settings_d.webp';
+import TShortenerD from './t_shortener_d.webp';
+
+export { THomeD, TLinkD, TRenamerD, TSettingsD, TShortenerD };
---
+++ src/shared/content/toolbox/mobile/index.ts
@@ -0,0 +1,7 @@
+import THomeM from './t_home_m.webp';
+import TLinkM from './t_links_m.webp';
+import TRenamerM from './t_renamer_m.webp';
+import TSettingsM from './t_settings_m.webp';
+import TShortenerM from './t_shortener_m.webp';
+
+export { THomeM, TLinkM, TRenamerM, TSettingsM, TShortenerM };
